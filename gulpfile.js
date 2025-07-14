3d99c595ee085945b85ab91af8d5e00d3f0c9ae2
ManjulaChintharla <manjula@technofocus.co>
2025-07-14 22:11:35 +0530

    Fix malformed package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,42 @@
-﻿- task: Npm@1
-  displayName: 'Run npm install'
-  inputs:
-    command: 'install'
-    verbose: false
+﻿/// <binding Clean='clean' />
+"use strict";
 
-- powershell: |
-    $scssPath = "Tailspin.SpaceGame.Web/wwwroot/scss"
-    if (Test-Path $scssPath) {
-      Write-Host "✅ SCSS directory found. Compiling..."
-      npx sass Tailspin.SpaceGame.Web/wwwroot/scss:Tailspin.SpaceGame.Web/wwwroot/css
-    } else {
-      Write-Host "⚠️ SCSS directory not found. Skipping Sass compilation."
-    }
-  displayName: 'Compile Sass assets'
+const gulp = require("gulp"),
+      rimraf = require("rimraf"),
+      concat = require("gulp-concat"),
+      cleanCSS = require("gulp-clean-css"),
+      uglify = require("gulp-uglify");
 
+const paths = {
+  webroot: "./Tailspin.SpaceGame.Web/wwwroot/"
+};
 
+paths.js = paths.webroot + "js/**/*.js";
+paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
 
-- script: 'npx gulp'
-  displayName: 'Run gulp tasks'
-  workingDirectory: Tailspin.SpaceGame.Web
+gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
+gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
+gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
 
+gulp.task("min:js", () => {
+  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    .pipe(concat(paths.concatJsDest))
+    .pipe(uglify())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", () => {
+  return gulp.src([paths.css, "!" + paths.minCss])
+    .pipe(concat(paths.concatCssDest))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("."));
+});
+
+gulp.task("min", gulp.series(["min:js", "min:css"]));
+
+// A 'default' task is required by Gulp v4
+gulp.task("default", gulp.series(["min"]));
