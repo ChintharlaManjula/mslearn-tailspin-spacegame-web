e2c0f879550b7ffdbde41d403e432c419a13cfc1
ManjulaChintharla <manjula@technofocus.co>
2025-07-14 22:05:46 +0530

    Fix malformed package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,22 @@
-﻿/// <binding Clean='clean' />
-"use strict";
+﻿- task: Npm@1
+  displayName: 'Run npm install'
+  inputs:
+    command: 'install'
+    verbose: false
 
-const gulp = require("gulp"),
-      rimraf = require("rimraf"),
-      concat = require("gulp-concat"),
-      cleanCSS = require("gulp-clean-css"),
-      uglify = require("gulp-uglify");
+- powershell: |
+    $scssPath = "Tailspin.SpaceGame.Web/wwwroot/scss"
+    if (Test-Path $scssPath) {
+      Write-Host "✅ SCSS directory found. Compiling..."
+      npx sass Tailspin.SpaceGame.Web/wwwroot/scss:Tailspin.SpaceGame.Web/wwwroot/css
+    } else {
+      Write-Host "⚠️ SCSS directory not found. Skipping Sass compilation."
+    }
+  displayName: 'Compile Sass assets'
 
-const paths = {
-  webroot: "./Tailspin.SpaceGame.Web/wwwroot/"
-};
+# ✅ Install gulp CLI globally
+- script: 'npm install --global gulp-cli'
+  displayName: 'Install Gulp CLI globally'
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
-
-gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
-gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
-gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
-
-gulp.task("min:js", () => {
-  return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-    .pipe(concat(paths.concatJsDest))
-    .pipe(uglify())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min:css", () => {
-  return gulp.src([paths.css, "!" + paths.minCss])
-    .pipe(concat(paths.concatCssDest))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min", gulp.series(["min:js", "min:css"]));
-
-// A 'default' task is required by Gulp v4
-gulp.task("default", gulp.series(["min"]));
+- task: gulp@1
+  displayName: 'Run gulp tasks'
